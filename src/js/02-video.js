c1a2803a70d7fd282e31f76c6873a8cc262f7cc4
ThisNarciss <tyomaxxx000@gmail.com>
2022-12-01 14:28:16 +0200

    create event timeupdate and set metod setcurrenttime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+const iframe = document.querySelector("iframe");
+const player = new Vimeo.Player(iframe);
 
+const onPlay = function (data) {
+  localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+};
+
+player.on("timeupdate", onPlay);
+
+player.getVideoTitle().then(function (title) {
+  console.log("title:", title);
+});
+
+const parsedData = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+console.log(parsedData);
+player
+  .setCurrentTime(parsedData.seconds)
+  .then(function (seconds) {
+    seconds = parsedData.seconds;
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case "RangeError":
+        parsedData.seconds < 0 || parsedData.seconds > parsedData.duration;
+        break;
+    }
+  });
