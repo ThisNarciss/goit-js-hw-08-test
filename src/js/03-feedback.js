8e0f563cdaa12df215e5cd8ac276c4f5e790363c
ThisNarciss <tyomaxxx000@gmail.com>
2022-12-01 19:27:10 +0200

    update hw 8 task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const formRef = document.querySelector(".feedback-form");
+const throttle = require("lodash.throttle");
 
+formRef.addEventListener("input", throttle(onTextareaInput, 500));
+formRef.addEventListener("submit", onFormSubmit);
+
+function onTextareaInput(event) {
+  const feedbackArr = {
+    email: event.currentTarget.elements.email.value,
+    message: event.currentTarget.elements.message.value,
+  };
+
+  localStorage.setItem("feedback-form-state", JSON.stringify(feedbackArr));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (
+    event.currentTarget.elements.email.value === "" ||
+    event.currentTarget.elements.message.value === ""
+  ) {
+    alert("Для відправки форми мають бути заповнені всі поля!!!");
+    return;
+  }
+  event.currentTarget.reset();
+}
+
+function fillInTextarea() {
+    const Data = localStorage.getItem("feedback-form-state");
+if (Data)
+    
+}
